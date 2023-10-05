144253e67829203836c09c0ee964abf6b96a9d8c
Fhelipe Rodrigues <contatofhelipe@gmail.com>
2023-10-05 18:02:06 -0300

    test: file mock

--- jest.config.ts
+++ jest.config.ts
@@ -90,7 +90,10 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '\\.(pdf|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>src/test/fileMock.ts',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
